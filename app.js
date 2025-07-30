984d49a79744d4b646b051b4136bb0fbf482cc3f
Samuel A. Fernandes <zamuelfernandes@gmail.com>
2025-07-30 16:43:01 -0300

    Lógica para exibir lista de amigos

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ function adicionarAmigo() {
   if (nome != '') {
     amigos.push(nome);
     document.getElementById('amigo').value = '';
+    listarAmigos();
   } else {
     alert('Por favor, insira um nome válido.');
   }
+}
+
+function listarAmigos() {
+  let lista = document.getElementById('listaAmigos');
+  lista.innerHTML = ''; // Limpa a lista antes de adicionar novos amigos
+  amigos.forEach(function(amigoNome) {
+    let item = document.createElement('li');
+    item.innerHTML = amigoNome;
+    console.log(`Adicionando amigo ${amigoNome} à lista.`);
+    lista.appendChild(item);
+  });
 }
